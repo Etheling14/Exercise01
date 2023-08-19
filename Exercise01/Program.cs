5d66997b2214089a6cfa98fa07358a13c50b893a
Urrea <Surrea1411@gmail.com>
2023-08-19 15:37:57 -0500

    Exercise 01

---
+++ Exercise01/Program.cs
@@ -0,0 +1,23 @@
+﻿do
+{
+    Console.WriteLine("Odd or Even!");
+    Console.WriteLine("============");
+    Console.Write("Enter a number: ");
+    var numberString = Console.ReadLine();
+    try
+    {
+        var numerInteger = Convert.ToInt32(numberString);
+        if (numerInteger % 2 == 0)
+        {
+            Console.WriteLine($"The number: {numerInteger} is odd.");
+        }
+        else
+        {
+            Console.WriteLine($"The number: {numerInteger} is even.");
+        }
+    }
+    catch (Exception)
+    {
+        Console.WriteLine($"The input: {numberString} is not a number.");
+    }
+} while (true);
